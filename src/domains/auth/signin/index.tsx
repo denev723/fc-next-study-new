ef60c19a145f51fbb7f5ee6f691494283debd41b
denev <denev723@gmail.com>
2025-09-22 21:18:01 +0900

    로그인 페이지 UI

---
+++ src/domains/auth/signin/index.tsx
@@ -0,0 +1,50 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { FC, FormEvent, useState } from "react";
+import styles from "./index.module.css";
+import { SHA256 } from "crypto-js";
+
+const SigninMain: FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert("모든 항목을 입력해주세요.");
+      return;
+    }
+
+    const password_hash = SHA256(password).toString();
+
+    console.log(email, password, password_hash);
+
+    // mutate({ name, email, password: password_hash });
+  };
+
+  return (
+    <main>
+      <section className={styles.section}>
+        <h1 className={styles.title}>로그인</h1>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <Input
+            id="email"
+            type="email"
+            placeholder="이메일"
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+          <Input
+            id="password"
+            type="password"
+            placeholder="비밀번호"
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
+          <Button type="submit">로그인</Button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export { SigninMain };
---
+++ src/pages/auth/signin/index.tsx
@@ -0,0 +1,8 @@
+import { SigninMain } from "@/domains/auth/signin";
+import { FC } from "react";
+
+const SigninPage: FC = () => {
+  return <SigninMain />;
+};
+
+export default SigninPage;
